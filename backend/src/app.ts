f74541ed6e9565c47c78dc27fa91a777790c2629
AryaPiyus <aryapiyush2930@gmail.com>
2025-12-14 20:36:40 +0530

    render

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin:  'https://sweet-shop-kata-eta.vercel.app/', 
   credentials: true 
 }));
 app.use(express.json());
